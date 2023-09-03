564c53d49c61181e5dade4f485dd6133f7aae4c2
Reizy <malfunzionatoo@proton.me>
2023-09-04 00:40:22 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <>
       <GenericMeta
-        title="Reizy"
-        description="Owner of Malfunzionato Records Independent"
+        title="Vaishnav"
+        description="Tech enthusiast and full stack developer constantly pushing the boundaries of what's possible."
       />
       <MainLayout margin={false}>
         <Hero />
