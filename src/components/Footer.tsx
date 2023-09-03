9538787c2bcbe547d520bd6bdd3ac13e87339b0d
Reizy <malfunzionatoo@proton.me>
2023-09-04 01:11:17 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -97,29 +97,6 @@ export default function Footer() {
               <polyline points="22,6 12,13 2,6"></polyline>
             </svg>
           </a>
-          <a
-            href="/linkedin"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-ghost"
-            aria-label="LinkedIn"
-          >
-            {' '}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              className="mx-2 hover:scale-110 fill-lightText hover:fill-butDark hover:text-butDark transition duration-200 ease-in-and-out"
-              width="30"
-              height="30"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
-            </svg>
-          </a>
-        </div>
         <div className="btn-group grid grid-cols-2 justify-between md:justify-end w-full rounded-md outline md:outline-2">
           <a
             href="/hackerrank"
