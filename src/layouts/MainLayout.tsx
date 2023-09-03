f4cee817e2d5b915ead0cee2ab4d2eee4873c908
Reizy <malfunzionatoo@proton.me>
2023-09-04 00:54:08 +0200

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -19,12 +19,12 @@ export const MainLayout = ({
   return (
     <>
       {title && description && (
-        <GenericMeta title={title} description={description} />
+        <GenericMeta title="Reizy" description="Owner of Malfunzionato Records Independent. Admin on Torta Factory Discord Server and Owner on the Minecraft server." />
       )}
 
       <FadeIn>
         {description && (
-          <p className="text-gray-600 dark:text-gray-400 mt-2">{description}</p>
+          <p className="text-gray-600 dark:text-gray-400 mt-2">"Owner of Malfunzionato Records Independent. Admin on Torta Factory Discord Server and Owner on the Minecraft server."</p>
         )}
         <div className={margin ? 'mt-8' : undefined}>{children}</div>
         <Footer />
