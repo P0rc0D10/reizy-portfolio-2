c81093a05d6a7d5b0785cd45f1d6ab0c837dd2a4
Reizy <malfunzionatoo@proton.me>
2023-09-04 00:43:24 +0200

    Update weather.ts

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=padova&units=metric&appid=${process.env.WEATHER_API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=padova&units=metric&appid=eecd2e62383a31c75535e0c23d2eb967`,
   )
   const data = await response.json()
   res.status(200).json(data)
