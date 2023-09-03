7f3e22f024063bd7e20901afc8801be6c3d92a8a
Reizy <malfunzionatoo@proton.me>
2023-09-04 01:04:41 +0200

    Update Weather.tsx

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -47,7 +47,7 @@ export const Weather = () => {
         target="_blank"
         className="font-bold focus:outline-none transition duration-300 ease-in-out hover:text-[#36D399]"
       >
-        Chennai
+        Padova, Veneto, Italy
       </a>
       .
     </motion.div>
