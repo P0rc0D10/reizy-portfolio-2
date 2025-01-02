5ec0a29161ae04b62a785cd4aad96d995efe451b
DisPaisy <malfunzionatoo@proton.me>
2025-01-02 03:03:43 +0100

    Update DefaultMeta.tsx

--- src/components/DefaultMeta.tsx
+++ src/components/DefaultMeta.tsx
@@ -11,7 +11,7 @@ export const GenericMeta = ({ title, description }: Props) => {
       <link rel="preconnect" href="https://vitals.vercel-insights.com" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <title>{title}</title>
-      <meta name="author" content="Reizy" />
+      <meta name="author" content="DisPaisy" />
       <meta name="theme-color" content="#36D399" />
       <meta property="og:title" content={title} />
       <meta name="description" content={description} />
@@ -19,11 +19,11 @@ export const GenericMeta = ({ title, description }: Props) => {
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Reizy" />
       <meta property="og:locale" content="en_US" />
-      <meta property="og:url" content="https://reizy.eu" />
+      <meta property="og:url" content="https://dis.paisy.lol" />
       <meta property="og:image" content="/profile.gif" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@reizyishere" />
-      <meta name="twitter:creator" content="@reizyishere" />
+      <meta name="twitter:site" content="@dispaisy" />
+      <meta name="twitter:creator" content="@dispaisy" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content="/profile.gif" />
